cb9c2a0495af84394740092645e09149847fed3a
Ghéczy Bálint <balintgheczy@gmail.com>
2025-04-09 11:01:18 +0200

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import Termek from "./Termek.js";
+import Termekek from "./Termekek.js";
 import { termekLista } from "./termekLista.js";
 import {kosarLista} from "./kosarLista.js";
 
 const pElem = document.querySelector('article');
 const ipElem = document.querySelector('aside');
+
+const TERMEKEK = new Termekek(pElem, ipElem, termekLista, kosarLista);
+
