ecd018585e448a6298978071a5c6f5845790360f
GyReka09 <gy.reka09@gmail.com>
2025-04-09 09:55:41 +0200

    listak

---
+++ termekLista.js
@@ -0,0 +1,32 @@
+const termekLista = [
+  {
+    kep: "malac.jpg",
+    nev: "Ten Geri",
+    leiras: "Kopasz tengerimalac",
+    ar: 16000,
+  },
+  {
+    kep: "images.jpeg",
+    nev: "Oszkár",
+    leiras: "Kopasz kutyus",
+    ar: 280000,
+  },
+  {
+    kep: "nyusznyusz.jpg",
+    nev: "Tamás",
+    leiras: "Kopasz nyúl",
+    ar: 40000,
+  },
+  {
+    kep: "minskin.jpg",
+    nev: "Viktória",
+    leiras: "kopasz cicus",
+    ar: 322000,
+  },
+  {
+    kep: "potkany.jpg",
+    nev: "Herold",
+    leiras: "kopasz potkány",
+    ar: 28000,
+  },
+];
