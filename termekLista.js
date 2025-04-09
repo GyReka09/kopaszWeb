746e3f9d0c4d54fc9dc080b12dc298a6525a6be4
GyReka09 <gy.reka09@gmail.com>
2025-04-09 09:58:10 +0200

    mappa

--- termekLista.js
+++ termekLista.js
@@ -1,30 +1,30 @@
 const termekLista = [
   {
-    kep: "malac.jpg",
+    kep: "kepek/malac.jpg",
     nev: "Ten Geri",
     leiras: "Kopasz tengerimalac",
     ar: 16000,
   },
   {
-    kep: "images.jpeg",
+    kep: "kepek/images.jpeg",
     nev: "Oszkár",
     leiras: "Kopasz kutyus",
     ar: 280000,
   },
   {
-    kep: "nyusznyusz.jpg",
+    kep: "kepek/nyusznyusz.jpg",
     nev: "Tamás",
     leiras: "Kopasz nyúl",
     ar: 40000,
   },
   {
-    kep: "minskin.jpg",
+    kep: "kepek/minskin.jpg",
     nev: "Viktória",
     leiras: "kopasz cicus",
     ar: 322000,
   },
   {
-    kep: "potkany.jpg",
+    kep: "kepek/potkany.jpg",
     nev: "Herold",
     leiras: "kopasz potkány",
     ar: 28000,
