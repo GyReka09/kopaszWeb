3aa8d6b6423e3cf0caa3a9c98f4b960876dad58e
Ghéczy Bálint <balintgheczy@gmail.com>
2025-04-09 11:15:07 +0200

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Termekek from "./Termekek.js";
 import { termekLista } from "./termekLista.js";
-import { kosarLista } from "./kosarLista.js";
+import { kosar } from "./kosarLista.js";
 
 const pElem = document.querySelector("article");
 const ipElem = document.querySelector("aside");
