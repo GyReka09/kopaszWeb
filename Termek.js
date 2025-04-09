ab61e81a24cbff8f7e0b7aa3a56b7da01726f585
GyReka09 <gy.reka09@gmail.com>
2025-04-09 10:15:40 +0200

    hscasdchm

--- Termek.js
+++ Termek.js
@@ -1,3 +1,29 @@
-class Termek {
-  constructor() {}
+export default class Termek {
+  #index;
+  constructor(pElem, termek, index) {
+    this.termek = termek;
+    this.#index = index;
+
+    this.view();
+    this.termElem = document.querySelector(".term:last-child");
+    this.kosElem = this.termElem.querySelector(".kosarg: last-child");
+    this.kosar();
+  }
+
+  view() {
+    let html = `<p class="text">${this.kep}
+                  <button class="ready">Ok</button>
+                  <button class="delete">Del</button>
+              </p>`;
+    this.pElem.insertAdjacentHTML("beforeend", html);
+  }
+
+  kosar() {
+    this.kosElem.addEventListener("click", (event) => {
+      console.log(this.#index);
+
+      const e = new CustomEvent("kosar", { detail: this.#index });
+      window.dispatchEvent(e);
+    });
+  }
 }
--- Termekek.js
+++ Termekek.js
@@ -1,4 +1,62 @@
-export default class Termekek {
+import Termek from "./Termek.js";
+import TodoInput from "./TodoInput.js";
 
-    
+export default class Termekek {
+  #list;
+  constructor(pElem, ipElem, list) {
+    this.#list = list;
+    this.pElem = pElem;
+    this.ipElem = ipElem;
+    this.viewInput();
+    this.viewTodos();
+    this.addEvent();
+    this.removeEvent();
+    this.readyEvent();
+  }
+
+  viewTodos() {
+    this.pElem.innerHTML = "";
+
+    for (let index = 0; index < this.#list.length; index++) {
+      if (this.#list[index].exists === true) {
+        const element = this.#list[index];
+        const TODO = new Todo(element.name, this.pElem, index);
+        if (this.#list[index].green === true) {
+          TODO.textElem.classList.add("green");
+        }
+      }
+    }
+  }
+
+  viewInput() {
+    new TodoInput(this.ipElem);
+  }
+
+  removeEvent() {
+    window.addEventListener("remove", (event) => {
+      this.pElem.innerHTML = "";
+      console.log(event.detail);
+      this.#list[event.detail].exists = false;
+      this.viewTodos();
+    });
+  }
+
+  addEvent() {
+    window.addEventListener("add", (event) => {
+      console.log(event.detail);
+      let y = this.#list.length + 1 + "." + event.detail;
+      let x = { name: y, exists: true, green: false };
+      this.#list.push(x);
+      this.viewTodos();
+    });
+  }
+
+  readyEvent() {
+    window.addEventListener("ready", (event) => {
+      this.pElem.innerHTML = "";
+      console.log(event.detail);
+      this.#list[event.detail].green = true;
+      this.viewTodos();
+    });
+  }
 }
